29d8ed2fdf924111aba36b0bfcc91baef17ffc93
Sorin Chis <kish.sorin@yahoo.com>
2019-01-21 13:41:38 +0200

    added object for each intern in the company list

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,6 +19,38 @@ const example = {
 
 // Write your intern objects here:
 
+const internMitzi = {
+    id: 1,
+    email: "mmelloy0@psu.edu",
+    first_name: "Mitzi",
+    gender: 'F',
+}
+const internKennan = {
+    id: 2,
+    email: "kdiben1@tinypic.com",
+    first_name: "Kennan",
+    gender: 'M',
+}
+const internKeven = {
+    id: 3,
+    email: "kmummery2@wikimedia.org",
+    first_name: "Keven",
+    gender: 'M',
+}
+
+const internGannie = {
+    id: 4,
+    email: "gmartinson3@illinois.edu",
+    first_name: "Gannie",
+    gender: 'F',
+}
+
+const internAntonietta = {
+    id: 5,
+    email: "adaine5@samsung.com",
+    first_name: "Antonietta",
+    gender: 'F',
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
