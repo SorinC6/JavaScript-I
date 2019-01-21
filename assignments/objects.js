96ef310adf5dc4a9946a36c0901d86db11d2c604
Sorin Chis <kish.sorin@yahoo.com>
2019-01-21 13:54:23 +0200

    method added to the objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -30,6 +30,9 @@ const internKennan = {
     email: "kdiben1@tinypic.com",
     first_name: "Kennan",
     gender: 'M',
+    speak: function(){
+        console.log("Hello, my name is Kennan!");
+    }
 }
 const internKeven = {
     id: 3,
@@ -50,6 +53,9 @@ const internAntonietta = {
     email: "adaine5@samsung.com",
     first_name: "Antonietta",
     gender: 'F',
+    multiplyNums: function(a,b){
+        console.log(a*b);
+    }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -77,10 +83,10 @@ console.log(internAntonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+ console.log(internKennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+console.log(internAntonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
